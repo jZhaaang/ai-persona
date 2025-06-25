4b64b14eda7a2c1e064ba547ad20c9b11d445541
Jimmy <jimmy.zhaang@gmail.com>
2025-06-24 22:36:03 -0400

    rename filtered to processed

--- scripts/config.py
+++ scripts/config.py
@@ -7,7 +7,7 @@ SCRIPTS_DIR = PROJECT_DIR / "scripts"
 
 RAW_DIR = DATA_DIR / "raw"
 CLEAN_DIR = DATA_DIR / "clean"
-FILTERED_DIR = CLEAN_DIR / "filtered"
+PROCESSED_DIR = CLEAN_DIR / "processed"
 CHUNKS_DIR = CLEAN_DIR / "chunks"
 EMBED_DIR = DATA_DIR / "embed"
 AUTHORS_PATH = DATA_DIR / "authors_map.json"
