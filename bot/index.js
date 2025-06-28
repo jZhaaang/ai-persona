eaebbb507f7c54c38f657ecc18f9b4d1b0a264fe
Jimmy <jimmy.zhaang@gmail.com>
2025-06-28 15:06:59 -0400

    remove loading commands

--- bot/index.js
+++ bot/index.js
@@ -1,22 +1,9 @@
 require("dotenv").config({ path: "../.env"});
 const fs = require("node:fs");
 const path = require("node:path")
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ]});
-
-// load commands
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsPath);
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`)
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(`[WARNING] The command in ${file} is missing required "data" or "execute" property.`)
-  }
-}
 
 // load events
 const eventsPath = path.join(__dirname, "events");
